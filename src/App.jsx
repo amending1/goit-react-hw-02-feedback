b6952d9f664bc5ef51589037cee1395679e80685
amending1 <joasia.sowiak@gmail.com>
2024-01-25 21:30:17 +0100

    Notification component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Section from './components/Section.jsx';
 import Statistics from 'components/Statistics.jsx';
 import FeedbackOptions from 'components/FeedbackOptions.jsx';
+import Notification from './components/Notification.jsx';
 import css from './components/FeedbackForm.module.css';
 
 export class App extends Component {
@@ -17,8 +18,10 @@ export class App extends Component {
 
   //   'prevState' to aktualny stan komponentu
   // [type] oznacza, że używam wartości zmiennej type jako klucza obiektu, co pozwala mi na dynamiczne określanie, którą właściwość stanu chcę zaktualizować
-  handleFeedback = (opinion) => {
-    this.setState(prevState => ({ [opinion]: prevState[opinion] + 1 }));
+  handleFeedback = opinion => {
+    this.setState(prevState => ({
+      [opinion]: prevState[opinion] + 1,
+    }));
   };
 
   render() {
@@ -30,23 +33,26 @@ export class App extends Component {
       //czy można polączyć jakoś te dwa divy poniżej?? jak opisać wtedy className??
       <div className="App">
         <div className={css['container-feedback-form']}>
-        <Section title="Please leave feedback"></Section>
-        <Section title="Feedback Options">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.handleFeedback} 
-            //funkcja handleFeedback, zdefiniowana w komponencie App, zostanie przekazana jako props do komponentu FeedbackOptions, aby mogła zostać wywołana w odpowiedzi na akcję użytkownika
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        </Section>
+          <Section title="Please leave feedback"></Section>
+          <Section title="Feedback Options">
+            <FeedbackOptions
+              options={this.state}
+              onLeaveFeedback={this.handleFeedback}
+              //funkcja handleFeedback, zdefiniowana w komponencie App, zostanie przekazana jako props do komponentu FeedbackOptions, aby mogła zostać wywołana w odpowiedzi na akcję użytkownika
+            />
+          </Section>
+          <Section title="Statistics">
+            {total > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                positivePercentage={positivePercentage}
+              />
+            ) : (
+            <Notification message="There is no feedback" />)}
+          </Section>
         </div>
       </div>
     );
